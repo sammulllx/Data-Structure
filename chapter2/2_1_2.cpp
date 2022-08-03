1a2d3e8a2d95b310fdac2104a03586e9b735ab04
sammulllx <2339576844@qq.com>
2022-08-03 16:06:03 +0800

    更新

--- chapter2/2_1_2.cpp
+++ chapter2/2_1_2.cpp
@@ -2,22 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define Max 50
-struct SqList {
-  int a[Max] = {1, 4, 6, 3, 2, 8, 7};
-  int length = 7;
+struct SqList
+{
+    int a[Max] = {1, 4, 6, 3, 2, 8, 7};
+    int length = 7;
 };
-void reverse(SqList &L) {
-  for (int i = 0; i < L.length / 2; i++) {
-    int v = L.a[i];
-    L.a[i] = L.a[L.length - i - 1];
-    L.a[L.length - i - 1] = v;
-  }
+void reverse(SqList &L)
+{
+    int temp;
+    for (int i = 0; i < L.length / 2; i++)
+    {
+        temp = L.a[i];
+        L.a[i] = L.a[L.length - i - 1];
+        L.a[L.length - i - 1] = temp;
+    }
 }
-int main() {
-  SqList L;
-  for (int i = 0; i < L.length; i++) cout << L.a[i] << " ";
-  cout << endl;
-  reverse(L);
-  for (int i = 0; i < L.length; i++) cout << L.a[i] << " ";
-  return 0;
+int main()
+{
+    SqList L;
+    for (int i = 0; i < L.length; i++)
+        cout << L.a[i] << " ";
+    cout << endl;
+    reverse(L);
+    for (int i = 0; i < L.length; i++)
+        cout << L.a[i] << " ";
+    return 0;
 }
--- chapter2/2_1_3.cpp
+++ chapter2/2_1_3.cpp
@@ -1,29 +1,33 @@
-#include<bits/stdc++.h>
+//删除顺序表中所有值为x的元素
+#include <bits/stdc++.h>
 using namespace std;
 #define Max 50
-struct  SqList
+struct SqList
 {
-    /* data */
-    int a[Max]={5,2,0,1,3,1,4,1,1,4};
-    int length=10;
+    int a[Max] = {5, 2, 0, 1, 3, 1, 4, 1, 1, 7};
+    int length = 10;
 };
-void deletelist(SqList &L,int x)
+void deletelist(SqList &L, int x)
 {
-    int k=0;
-    for(int i=0;i<L.length;i++)
+    int k = 0;
+    for (int i = 0; i < L.length; i++)
     {
-        if(L.a[i]==x) k++;
-        else L.a[i-k]=L.a[i];
+        if (L.a[i] == x)
+            k++;
+        else
+            L.a[i - k] = L.a[i];
     }
-    L.length=L.length-k;
+    L.length = L.length - k;
 }
 int main()
 {
     SqList L;
-    for(int i=0;i<L.length;i++) cout<<L.a[i]<<" ";
-    cout<<endl;
-    deletelist(L,1);
-    for(int i=0;i<L.length;i++) cout<<L.a[i]<<" ";
-    cout<<endl;
+    for (int i = 0; i < L.length; i++)
+        cout << L.a[i] << " ";
+    cout << endl;
+    deletelist(L, 1);
+    for (int i = 0; i < L.length; i++)
+        cout << L.a[i] << " ";
+    cout << endl;
     return 0;
 }
