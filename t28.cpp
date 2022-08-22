af9a2cd07839e9157db18f4e0ab13d9291872fa2
sammulllx <2339576844@qq.com>
2022-08-23 01:47:05 +0800

    8-23-01

---
+++ t28.cpp
@@ -0,0 +1,85 @@
+//合并A，B链表为非递减有序表C
+#include <iostream>
+using namespace std;
+typedef struct LNode
+{
+    int data;
+    struct LNode *next;
+} LNode;
+
+void buildlist(LNode *&l, int a[], int n)
+{
+    l = (LNode *)malloc(sizeof(LNode));
+    LNode *s, *r = l;
+    for (int i = 0; i < n; i++)
+    {
+        s = (LNode *)malloc(sizeof(LNode));
+        s->data = a[i];
+        r->next = s;
+        r = r->next;
+    }
+    r->next = NULL;
+}
+
+void showlist(LNode *L)
+{
+    if (L)
+    {
+        LNode *s = L->next;
+        while (s)
+        {
+            cout << s->data << " ";
+            s = s->next;
+        }
+        cout << endl;
+    }
+}
+
+void merge(LNode *A, LNode *B, LNode *&C)
+{
+    LNode *p = A->next;
+    LNode *q = B->next;
+    C = A;
+    C->next = NULL;
+    free(B);
+    LNode *r;
+    r = C;
+    while (p != NULL && q != NULL)
+    {
+        if (p->data <= q->data)
+        {
+            r->next = p;
+            p = p->next;
+            r = r->next;
+        }
+        else
+        {
+            r->next = q;
+            q = q->next;
+            r = r->next;
+        }
+    }
+    r->next = NULL;
+    if (p != NULL)
+        r->next = p;
+    if (q != NULL)
+        r->next = q;
+}
+int main()
+{
+    int n = 8;
+    int a[8] = {1, 4, 5, 7, 8, 11, 13, 14};
+    int b[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    LNode *A, *B, *C;
+    buildlist(A, a, n);
+    buildlist(B, b, n);
+    showlist(A);
+    showlist(B);
+    merge(A, B, C);
+
+    showlist(C);
+    int *p = NULL;
+    printf("%d", (int)p);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-int main()
+
+int main(void)
 {
-    int m = 1;
-    for (int i = 0; i < 1; i++)
-    {
-        m++;
-    }
-    printf("%d", m);
-    char word[101 / 2] = {"hello"};
+    char m[40];
+    char n;
+    printf("please input first str:\n");             //提示用户输入第一个字符串
+    scanf("%s", m);                                  //获取用户第一个输入字符串
+    printf("you input str is :%s\n", m);             //输出用户的输入的第一个字符串
+    printf("input second char :\n");                 //提示用户输入第二个字符
+    scanf("%c", &n);                                 //获取用户的第二个字符
+    printf("now you input second char is :%c\n", n); //输出用户输入的第二个字符
+    printf("hello");
+    printf("return");
+    printf("printfhello-world");
+    printf("hello");
+    prtinf("sword");
+    return 0;
 }
